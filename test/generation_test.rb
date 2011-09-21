63f147fcd0b3adc583aad9d8f4512b266dd58442
Christoph Olszowka <christoph@olszowka.de>
2011-09-21 22:10:17 +0200

    Fixed test suite

--- test/generation_test.rb
+++ test/generation_test.rb
@@ -2,11 +2,15 @@ require 'test_helper'
 
 class GenerationTest < Test::Unit::TestCase
   def test_generation
-    10_000.times do
-      name = GrumpyHerring.next
+    while name = GrumpyHerring.next
       assert name.instance_of?(String)
       assert_equal 2, name.split(' ').length
-      puts name
+      if name == 'Grumpy Herring'
+        puts name.upcase
+        break
+      else
+        puts name
+      end
     end
   end
   
